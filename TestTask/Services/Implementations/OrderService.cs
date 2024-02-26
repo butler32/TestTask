796e03e98ddd717e3fc5768040c670782be407c6
Владимир Дворецкий <plus88228@gmail.com>
2024-02-26 15:53:16 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var maxValue = await _context.Orders.MaxAsync(p => p.Price);
+            var orderWithMaxPrice = await _context.Orders.FirstOrDefaultAsync(order => order.Price == maxValue);
+
+            return orderWithMaxPrice;
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _context.Orders.Where(q => q.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var userWithMaxOrders = await _context.Users
+                .Include(o => o.Orders)
+                                    .OrderByDescending(u => u.Orders.Count)
+                                    .FirstOrDefaultAsync();
+
+            return userWithMaxOrders;
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _context.Users.Where(s => s.Status == Enums.UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
